1d42b9ef26ac1d9ef1e3c1fc3ccb8dcb64b7dca3
Binaya shrestha <sthavicky555@gmail.com>
2025-07-03 19:28:58 +0545

    YOLO for webcam and videos

---
+++ yolo_webcam.py
@@ -0,0 +1,51 @@
+from ultralytics import YOLO
+import cv2 as cv
+import math
+
+classNames = ["person", "bicycle", "car", "motorbike", "aeroplane", "bus", "train", "truck", "boat",
+              "traffic light", "fire hydrant", "stop sign", "parking meter", "bench", "bird", "cat",
+              "dog", "horse", "sheep", "cow", "elephant", "bear", "zebra", "giraffe", "backpack", "umbrella",
+              "handbag", "tie", "suitcase", "frisbee", "skis", "snowboard", "sports ball", "kite", "baseball bat",
+              "baseball glove", "skateboard", "surfboard", "tennis racket", "bottle", "wine glass", "cup",
+              "fork", "knife", "spoon", "bowl", "banana", "apple", "sandwich", "orange", "broccoli",
+              "carrot", "hot dog", "pizza", "donut", "cake", "chair", "sofa", "pottedplant", "bed",
+              "diningtable", "toilet", "tvmonitor", "laptop", "mouse", "remote", "keyboard", "cell phone",
+              "microwave", "oven", "toaster", "sink", "refrigerator", "book", "clock", "vase", "scissors",
+              "teddy bear", "hair drier", "toothbrush"
+              ]
+
+#cap = cv.VideoCapture(0) #for webcam
+
+cap = cv.VideoCapture("Learn_YOLO\Videos\\3.mp4") #for videos
+
+model = YOLO('yolov8n.pt')
+
+while True:
+    ret, img = cap.read()
+    frame = cv.resize(img, (1380, 720))
+
+    result = model(frame, stream=True)
+    for r in result:
+        boxes = r.boxes
+        for box in boxes:
+            #bounding box
+            x1, y1, x2, y2 = box.xyxy[0]
+            x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
+            # print(x1, y1, x2, y2)
+            cv.rectangle(frame, (x1, y1), (x2,y2), (0, 255, 0), 2)
+
+            #confidence
+            conf = math.ceil((box.conf[0] * 100)) / 100
+            # print(conf)
+
+            #classlabel
+            cls = int(box.cls[0])
+            cv.putText(frame, f'{classNames[cls]}: {conf}', (max(0, x1), max(40, y1)), cv.FONT_HERSHEY_PLAIN, 2, (0, 255, 0), 2, cv.LINE_4)
+
+    cv.imshow("webcam", frame)
+    if cv.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv.destroyAllWindows()
+
